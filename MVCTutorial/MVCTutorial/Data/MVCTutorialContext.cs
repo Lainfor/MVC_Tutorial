d8e13b8ead0d740c4b81d0499c49c879726409d0
Lainfor <rmlainfor@gmail.com>
2018-10-14 23:03:31 +0100

    Tutorial completo
    Tutorial completo

---
+++ MVCTutorial/MVCTutorial/Data/MVCTutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCTutorial.Models
+{
+    public class MVCTutorialContext : DbContext
+    {
+        public MVCTutorialContext (DbContextOptions<MVCTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCTutorial.Models.Movie> Movie { get; set; }
+    }
+}
